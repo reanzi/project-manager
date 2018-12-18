6080bc42fcbd89350ece212e10dbab41485a4b73
reanzi <nziku99@gmail.com>
2018-12-18 22:09:09 +0300

    Created a central store using Redux

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-const ProjectSummary = () => {
+const ProjectSummary = ({ project }) => {
   return (
     <div className="project-list section">
       <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
+          <span className="card-title">{project.title}</span>
+          <p>{project.content}</p>
           <p>
             Posted by <span className="text-text">Rahel Nziku</span>
           </p>
@@ -16,4 +17,4 @@ const ProjectSummary = () => {
   );
 };
 
-export default ProjectSummary
+export default ProjectSummary;
