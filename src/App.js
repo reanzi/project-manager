e15e9a19b82abd9b494b4109ea15a1a20712539d
reanzi <nziku99@gmail.com>
2018-12-18 16:35:21 +0300

    Synced with mac; cloned into mac from cloud

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
-
 class App extends Component {
   render() {
     return (
@@ -10,7 +9,7 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route path="/" component={Dashboard} />
+            <Route exact path="/" component={Dashboard} />
           </Switch>
         </div>
       </BrowserRouter>
