5c0c257b2447ac206638f9e061d3c138e30f6b4b
reanzi <nziku99@gmail.com>
2018-12-18 17:22:54 +0300

    Created Signin and Signup forms, done

---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+
+export default class SignUp extends Component {
+  state = {
+    firstName: "",
+    LastName: "",
+    email: "",
+    password: ""
+  };
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+  render() {
+    return (
+      <div className="container">
+        <form className="white" onSubmit={this.handleSubmit} autoComplete="off">
+          <h5 className="grey-text text-darken-3">
+            Sign Up -- Join Us for Free
+          </h5>
+
+          <div className="input-field">
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              id="firstName"
+              onChange={this.handleChange}
+              value={this.state.firstName}
+            />
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              id="lastName"
+              onChange={this.handleChange}
+              value={this.state.lasttName}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              onChange={this.handleChange}
+              value={this.state.email}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              onChange={this.handleChange}
+              value={this.state.password}
+            />
+          </div>
+          <div className="input-field">
+            <button className="btn pink ligthen-1 z-depth-0">Join</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
