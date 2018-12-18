5556f8d08c385eb6c85628ec381bab07ebe2aa6f
reanzi <nziku99@gmail.com>
2018-12-19 00:35:42 +0300

    connected to the firebase, can add data now

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,21 @@
 export const createProject = project => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     //make async call to db
-    dispatch({ type: "CREATE_PROJECT", project });
+    const db = getFirestore();
+    db.collection("projects")
+      .add({
+        ...project,
+        authorFirstName: "Rechol",
+        authorLastName: "Raymond",
+        authorId: 323281,
+        createdAt: new Date()
+      })
+      .then(() => {
+        // dispatch
+        dispatch({ type: "CREATE_PROJECT", project });
+      })
+      .catch(err => {
+        dispatch({ type: "CREATE_PROJECT_ERROR", err });
+      });
   };
 };
